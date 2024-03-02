4a09de04d0bb49f05605f621a386cf889285887c
krozfu <carlosuefagh@gmail.com>
2024-03-01 19:46:47 -0500

    Codigo copiado de https://github.com/hexojs/hexo para la actividad de software 3

---
+++ test/scripts/tags/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Tags', () => {
+  require('./asset_img');
+  require('./asset_link');
+  require('./asset_path');
+  require('./blockquote');
+  require('./code');
+  require('./full_url_for');
+  require('./iframe');
+  require('./img');
+  require('./include_code');
+  require('./link');
+  require('./post_link');
+  require('./post_path');
+  require('./pullquote');
+  require('./url_for');
+});
